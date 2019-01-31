172e37d45f807802828e1729a0de7e0fd121c3db
Paramvir Dehal <psdehal@lbl.gov>
2019-01-30 19:20:16 -0800

    fixed report name

--- lib/kb_mash/kb_object_utils/KBObjectUtils.py
+++ lib/kb_mash/kb_object_utils/KBObjectUtils.py
@@ -137,7 +137,7 @@ class KBObjectUtils:
                 {'direct_html_link_index': 0,
                  'html_links': [{'shock_id': dfuout['shock_id'],
                                  'name': 'index.html',
-                                 'label': 'Sourmash search results'}
+                                 'label': 'Mash search results'}
                                 ],
                  'report_object_name': 'kb_sourmash_report_' + str(uuid.uuid4()),
                  'workspace_name': wsname
